273c65605ecc0d9a5c1bebe207f6024a107f0669
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-05-13 06:49:02 +0100

    feat: added moviedb respose and mappers

---
+++ lib/infrastructure/movie_mapper/movie_mapper.dart
@@ -0,0 +1,25 @@
+import 'package:cinemapedia/domain/entities/movie_entity.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMaper {
+  static MovieEntity movieDbToEntity(MovieMovieDB movieDB) => MovieEntity(
+        adult: movieDB.adult,
+        backdropPath: (movieDB.backdropPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${movieDB.backdropPath}'
+            : 'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg',
+        genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
+        id: movieDB.id,
+        originalLanguage: movieDB.originalLanguage,
+        originalTitle: movieDB.originalTitle,
+        overview: movieDB.overview,
+        popularity: movieDB.popularity,
+        posterPath: (movieDB.posterPath != '')
+            ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}'
+            : 'no-post',
+        releaseDate: movieDB.releaseDate,
+        title: movieDB.title,
+        video: movieDB.video,
+        voteAverage: movieDB.voteAverage,
+        voteCount: movieDB.voteCount,
+      );
+}
