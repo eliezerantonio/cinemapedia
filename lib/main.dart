996dc7b4d4efc2895a3cecb544cfce076e7d4442
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-05-13 06:45:24 +0100

    feat:added ProviderScop on main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import './config/theme/app_theme.dart';
 import './config/router/app_router.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const ProviderScope(child: MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
