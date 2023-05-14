810853e3c2191df21b259b34518b6fcd46d221c6
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-05-14 18:14:55 +0100

    feat (MoviesDataSource): cration  getPopular mothod

--- lib/data/datasources/movies_datasource.dart
+++ lib/data/datasources/movies_datasource.dart
@@ -2,4 +2,8 @@ import '/domain/entities/movie_entity.dart';
 
 abstract class MovieDatasource {
   Future<List<MovieEntity>> getNowPlay({int page = 1});
+
+  
+  Future<List<MovieEntity>> getPopular({int page = 1});
+
 }
