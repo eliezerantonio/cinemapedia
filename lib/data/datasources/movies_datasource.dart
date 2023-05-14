5e8a8b6b9db6011ec842ca0c61b62d3e44646785
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-05-14 19:09:20 +0100

    feat (MoviesDatsource):  creation getUpcoming movies

--- lib/data/datasources/movies_datasource.dart
+++ lib/data/datasources/movies_datasource.dart
@@ -3,7 +3,7 @@ import '/domain/entities/movie_entity.dart';
 abstract class MovieDatasource {
   Future<List<MovieEntity>> getNowPlay({int page = 1});
 
-  
   Future<List<MovieEntity>> getPopular({int page = 1});
 
+  Future<List<MovieEntity>> getUpcoming({int page = 1});
 }
