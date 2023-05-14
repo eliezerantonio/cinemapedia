d49382721a5c30cea0293910b7818d2d3b76af72
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-05-14 18:15:34 +0100

    feat (MoviesRepository): creation getPopularMovies method

--- lib/data/repositories/movies_repository.dart
+++ lib/data/repositories/movies_repository.dart
@@ -2,4 +2,6 @@ import '/domain/entities/movie_entity.dart';
 
 abstract class MoviesRepository {
   Future<List<MovieEntity>> getNowPlaying({int page = 1});
+
+  Future<List<MovieEntity>> getPopular({int page = 1});
 }
