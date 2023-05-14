e608b4e4b3f38c334c65e5e51f9f0f0674f54add
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-05-14 18:18:29 +0100

    feat (MovieRepositoryImpl): implemeantion MovieDatasource

--- lib/infrastructure/repositories/movie_respository_impl.dart
+++ lib/infrastructure/repositories/movie_respository_impl.dart
@@ -4,12 +4,16 @@ import 'package:cinemapedia/domain/entities/movie_entity.dart';
 import '../../data/repositories/movies_repository.dart';
 
 class MovieRepositoryImpl implements MoviesRepository {
-
   final MovieDatasource datasource;
   MovieRepositoryImpl(this.datasource);
 
   @override
   Future<List<MovieEntity>> getNowPlaying({int page = 1}) {
     return datasource.getNowPlay(page: page);
+  }
+
+  @override
+  Future<List<MovieEntity>> getPopular({int page = 1}) {
+    return datasource.getPopular(page: page);
   }
 }
