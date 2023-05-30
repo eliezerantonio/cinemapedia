5f2bf50f0b3c19b4a902efd407a5e0ec3d10bb67
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-05-30 06:38:58 +0100

    feat(AppRouter): add new router- movie-screen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -6,18 +6,19 @@ final appRouter = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(
-      path: '/',
-      name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
-    ),
-    GoRoute(
-      path: '/movie/:id',
-      name: MovieScreen.name,
-      builder: (context, state) {
-        final movieId = state.pathParameters['id'] ?? 'no-id';
+        path: '/',
+        name: HomeScreen.name,
+        builder: (context, state) => const HomeScreen(),
+        routes: [
+          GoRoute(
+            path: 'movie/:id',
+            name: MovieScreen.name,
+            builder: (context, state) {
+              final movieId = state.pathParameters['id'] ?? 'no-id';
 
-        return MovieScreen(movieId: movieId);
-      },
-    )
+              return MovieScreen(movieId: movieId);
+            },
+          )
+        ]),
   ],
 );
