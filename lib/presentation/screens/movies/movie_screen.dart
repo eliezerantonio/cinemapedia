58a4279566bcb0e5088cc7ba46bcad054ba254db
eliezerantonio <eliezerfernandoantonio@gmail.com>
2023-06-18 03:25:52 +0100

    feat: add custom gradiante

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -185,6 +185,16 @@ class _CustomSliverApBar extends StatelessWidget {
       backgroundColor: Colors.black,
       expandedHeight: size.height * 0.7,
       foregroundColor: Colors.white,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          // icon: const Icon(Icons.favorite_border),
+          icon: const Icon(
+            Icons.favorite_border,
+            color: Colors.red,
+          ),
+        )
+      ],
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         title: Text(
@@ -205,36 +215,63 @@ class _CustomSliverApBar extends StatelessWidget {
                 },
               ),
             ),
-            const SizedBox.expand(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    end: Alignment.bottomCenter,
-                    begin: Alignment.topCenter,
-                    stops: [0.7, 1.0],
-                    colors: [
-                      Colors.transparent,
-                      Colors.black87,
-                    ],
-                  ),
-                ),
-              ),
+            const _CustomGradiante(
+              end: Alignment.bottomCenter,
+              begin: Alignment.topCenter,
+              stops: [0.7, 1.0],
+              colors: [
+                Colors.transparent,
+                Colors.black87,
+              ],
             ),
-            const SizedBox.expand(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    stops: [0.0, 0.3],
-                    colors: [
-                      Colors.black87,
-                      Colors.transparent,
-                    ],
-                  ),
-                ),
-              ),
+            const _CustomGradiante(
+              end: Alignment.bottomLeft,
+              begin: Alignment.topRight,
+              stops: [0.0, 0.2],
+              colors: [
+                Colors.black54,
+                Colors.transparent,
+              ],
+            ),
+            const _CustomGradiante(
+              begin: Alignment.topLeft,
+              stops: [0.0, 0.3],
+              colors: [
+                Colors.black87,
+                Colors.transparent,
+              ],
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomGradiante extends StatelessWidget {
+  final AlignmentGeometry? begin;
+  final AlignmentGeometry? end;
+  final List<double> stops;
+  final List<Color> colors;
+
+  const _CustomGradiante({
+    this.begin = Alignment.centerLeft,
+    this.end = Alignment.centerRight,
+    required this.stops,
+    required this.colors,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: begin!,
+            end: end!,
+            stops: stops,
+            colors: colors,
+          ),
         ),
       ),
     );
--- lib/presentation/views/home_views/home_view.dart
+++ lib/presentation/views/home_views/home_view.dart
@@ -1,7 +1,8 @@
-import 'package:cinemapedia/presentation/providers/providers.dart';
-import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '/presentation/providers/providers.dart';
+import '/presentation/widgets/widgets.dart';
 
 class HomeView extends ConsumerStatefulWidget {
   const HomeView({super.key});
